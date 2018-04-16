0d5227381e9b96e4d488b830d76c0937f205152d
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-16 21:15:33 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,11 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
+<<<<<<< HEAD
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+=======
+def 
+and I am learning to program in #{language}"
+>>>>>>> 980187d8dd9ef5424366addcd520b3c38bc03627
